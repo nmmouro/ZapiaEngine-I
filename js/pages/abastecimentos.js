5f6f65ae1f4dd71421d262f1c6e4ad3c55091136
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-08-31 20:39:57 -0300

    Add files via upload

---
+++ js/pages/abastecimentos.js
@@ -0,0 +1,22 @@
+import { createModule } from "../engine/module.js";
+import { SCHEMA_ABASTECIMENTOS } from "../engine/schema.js";
+
+export function iniciarAbastecimentos() {
+    const modulo = createModule({
+        entity: "abastecimentos",
+        schema: SCHEMA_ABASTECIMENTOS,
+        container: "#app",
+        stateName: "abastecimentos",
+        options: {
+            titulo: "Controle de Abastecimentos",
+            tabela: "Abastecimentos Registrados",
+            permitirNovo: true,
+            permitirEditar: true,
+            permitirExcluir: true,
+            pageSize: 10
+        }
+    });
+    window.abastecimentos = modulo;
+    return modulo;
+}
+
